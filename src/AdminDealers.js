895e9d700763f20ca857e7aa0029f782291a57c0
GTM4Health <129034982+GTM4Health@users.noreply.github.com>
2023-07-24 19:10:01 +0530

    Install Projects & Enhance UI v1.2.19

--- src/AdminDealers.js
+++ src/AdminDealers.js
@@ -27,7 +27,7 @@ const Dealers = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      await axios.post('http://localhost:5000/api/admin/dashboard/Dealers', {
+      await axios.post(`${process.env.REACT_APP_BASE_URL}/api/admin/dashboard/Dealers`, {
         name,
         web,
         address,
