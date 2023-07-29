0c5528f8a5227f80e2b2fff84d0b19fcbc6c115f
Developer <info@gtm4health.com>
2023-07-29 17:27:55 +0530

    Downloads Add & Fix

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -2,7 +2,7 @@
 // GTM4Health logo and In Private Beta Image
 //
 import React from "react";
-import imgP from "../images/logo.png";
+import imgP from "../images/newlogo.png";
 import Beta from "../images/beta.png";
 
 const Logo = () => {
