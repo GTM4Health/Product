f587c930c9ac76578fc1e182f1cb56a68d167fab
Developer <info@gtm4health.com>
2023-10-11 18:47:19 +0530

    Fix:User-Dashboard -v1.4.2

--- frontend/src/admin/users/AdminUserAccess.js
+++ frontend/src/admin/users/AdminUserAccess.js
@@ -73,6 +73,65 @@ function AdminDashboard() {
             <canvas id="myPieChart"></canvas> {/* Canvas element for the chart */}
           </div>
         </div>
+        <div className="page-title">
+            <h1 className="page-title-child">User Dashboard</h1>
+          </div>
+          <div className="page-display">
+            <h4 className="total-rows">Total Users = {totalRows}</h4>
+            <h4 className="right"><i>Displaying Page {currentPage} of {totalPages}</i></h4>
+          </div>
+          <div className="pagination-buttons">
+            {!isFirstPage && (
+              <button className="prev-button" onClick={handlePrevPage}>
+                &laquo; Prev
+              </button>
+            )}
+            {!isLastPage && (
+              <button className="next-button" onClick={handleNextPage}>
+                Next &raquo;
+              </button>
+            )}
+          </div>
+          <div className="table-content">
+            <table className="user-table">
+              <thead>
+                <tr>
+                  <th>Sl No.</th>
+                  <th>Name</th>
+                  <th>Email</th>
+                  <th>Phone</th>
+                  <th>Role</th>
+                  <th>Activated Time</th>
+                  {/* Add more table headers if needed */}
+                </tr>
+              </thead>
+              <tbody>
+                {users.map((user, index) => (
+                  <tr key={user._id}>
+                    <td>{(currentPage - 1) * pageSize + index + 1}</td>
+                    <td>{user.name}</td>
+                    <td>{user.email}</td>
+                    <td>{user.phone}</td>
+                    <td>{user.role}</td>
+                    <td>{user.activationTime}</td>
+                    {/* Add more table cells for additional features */}
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+          <div className="pagination-buttons">
+            {!isFirstPage && (
+              <button className="prev-button" onClick={handlePrevPage}>
+                &laquo; Prev
+              </button>
+            )}
+            {!isLastPage && (
+              <button className="next-button" onClick={handleNextPage}>
+                Next &raquo;
+              </button>
+            )}
+          </div>
       </div>
       <Footer />
     </div>
