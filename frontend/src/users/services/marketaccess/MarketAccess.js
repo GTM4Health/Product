370a36413b4f821277100278d3fdcf68de0d4265
Developer <info@gtm4health.com>
2023-11-16 18:57:22 +0530

    Fix : Dashboard changes & Bugs resolved

--- frontend/src/users/services/marketaccess/MarketAccess.js
+++ frontend/src/users/services/marketaccess/MarketAccess.js
@@ -196,7 +196,7 @@ const MarketAccess = () => {
                   <th>Infrastructure & Services</th>
                   <th>State</th>
                   <th>City</th>
-                  <th>Doctor Name</th>
+                  {/* <th>Doctor Name</th> */}
                   <th>Specialization</th>
                   {/* <th>Contact Email</th>
                   <th>Contact Number</th> */}
@@ -210,7 +210,7 @@ const MarketAccess = () => {
                     <td>{hospital.infraSer}</td>
                     <td>{hospital.state}</td>
                     <td>{hospital.city}</td>
-                    <td>{hospital.docName}</td>
+                    {/* <td>{hospital.docName}</td> */}
                     <td>{hospital.docSpez}</td>
                     {/* <td>{hospital.mail}</td>
                     <td>{hospital.phone}</td> */}
