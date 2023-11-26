2ef066134fabc75dbe142267f2d5fca90df7da68
Developer <info@gtm4health.com>
2023-11-26 18:56:06 +0530

    View CI Add

--- Backend/routes/intel.js
+++ Backend/routes/intel.js
@@ -25,4 +25,27 @@ router.post('/add-competitive-intelligence', async (req, res) => {
   }
 });
 
+// Additional route to fetch competitive intelligence data with pagination
+router.get('/competitive-intelligence', async (req, res) => {
+  try {
+    const { page, limit } = req.query;
+    const skip = (page - 1) * limit;
+
+    const competitiveData = await Intel.find().skip(skip).limit(parseInt(limit));
+    const totalRows = await Intel.countDocuments();
+
+    const totalPages = Math.ceil(totalRows / limit);
+
+    res.json({
+      competitiveData,
+      totalRows,
+      totalPages,
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+
 module.exports = router;
--- frontend/src/admin/services/intel/AdminViewCI.js
+++ frontend/src/admin/services/intel/AdminViewCI.js
@@ -1,53 +1,132 @@
 import React, { useState, useEffect } from 'react';
-import { Link } from 'react-router-dom';
-import Header2 from '../../../layout/users/Header2';
-import useAuth from '../../../hooks/useAuth';
 import Footer from '../../../layout/pages/Footer';
+import AdminMenuBar from '../../../layout/admin/AdminMenubar';
+import useAuth from '../../../hooks/useAuth';
+import AdminHeader from '../../../layout/admin/AdminHeader';
+import axios from 'axios';
 
-const AdminViewCI = () => {
+const CompetitiveIntelligencePortal = () => {
   const isAuthenticated = useAuth();
-  const [user, setUser] = useState(null);
-  const quotes = [
-    "This page is currently under Construction. Stay tuned!"
-    // "The page you're looking for seems to have taken a little wellness break.",
-    // "Oops! It looks like the link you followed is on a temporary vacation.",
-    // "We apologize, but it seems the digital stethoscope couldn't locate this page.",
-    // "404 – Page not found. Our team is working hard to diagnose and fix this issue.",
-    // "This page might have made a rounds to other dimensions. We're working to bring it back.",
-  ];
-  const [randomQuote, setRandomQuote] = useState("");
-
+  const [competitiveData, setCompetitiveData] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [pageSize, setPageSize] = useState(10);
+  const [totalRows, setTotalRows] = useState(0);
+  const [totalPages, setTotalPages] = useState(0);
 
   useEffect(() => {
-    const storedUser = JSON.parse(localStorage.getItem('user'));
-    if (storedUser) {
-      setUser(storedUser);
+    if (isAuthenticated) {
+      fetchCompetitiveIntelligence();
     }
-    const randomIndex = Math.floor(Math.random() * quotes.length);
-    setRandomQuote(quotes[randomIndex]);
-  }, []);
-  
-  if(!isAuthenticated){
-    return null;
-}
+  }, [isAuthenticated, currentPage]);
+
+  const fetchCompetitiveIntelligence = async () => {
+    try {
+      const response = await axios.get(`${process.env.REACT_APP_BASE_URL}/api/admin/dashboard/competitive-intelligence`, {
+        params: {
+          page: currentPage,
+          limit: pageSize,
+          // Add other parameters if needed
+        },
+      });
+      setCompetitiveData(response.data.competitiveData);
+      setTotalRows(response.data.totalRows);
+      setTotalPages(response.data.totalPages);
+    } catch (error) {
+      console.error(error);
+      // Handle error as needed
+    }
+  };
+
+  const isFirstPage = currentPage === 1;
+  const isLastPage = currentPage === totalPages;
+
+  const handlePrevPage = () => {
+    if (!isFirstPage) {
+      setCurrentPage(currentPage - 1);
+      window.scrollTo(0, 0);
+    }
+  };
+
+  const handleNextPage = () => {
+    if (!isLastPage) {
+      setCurrentPage(currentPage + 1);
+      window.scrollTo(0, 0);
+    }
+  };
+
 
   return (
-    <>
-    <Header2 user={user} />
-    <div className="not-found-page">
-      <div className="not-found-content">
-        <h5 className="not-found-title">Coming Soon!</h5>
-        <p className="not-found-description">
-          {randomQuote}
-        </p>
-        <Link to="/admin/dashboard" className="not-found-link">
-          <button className="not-found-button">Return to Dashboard</button>
-        </Link>
+    <div className="page-view">
+      <AdminHeader />
+      <div className="d-content">
+        <div className="dashboard">
+          <AdminMenuBar />
+          <div className="page-title">
+            <h1 className="page-title-child hdblue-tag">Competitive Intelligence Portal</h1>
+          </div>
+          {/* Add your filter and display components here */}
+          <div className="filter-container">
+            {/* Add filters as needed */}
+          </div>
+          <div className="page-display">
+            <h4 className="total-rows ft5">Total Competitive Intelligence Records = {totalRows}</h4>
+            <h4 className="right ft5">
+              <i>
+                Displaying Page {currentPage} of {totalPages}
+              </i>
+            </h4>
+          </div>
+          <div className="pagination-buttons">
+            {!isFirstPage && (
+              <button className="prev-button" onClick={handlePrevPage}>
+                &laquo; Prev
+              </button>
+            )}
+            {!isLastPage && (
+              <button className="next-button" onClick={handleNextPage}>
+                Next &raquo;
+              </button>
+            )}
+          </div>
+          <div className="table-content">
+            <table className="user-table">
+              <thead>
+                <tr>
+                  <th>Sl No.</th>
+                  <th>Domain/Product</th>
+                  <th>Competitor Info</th>
+                  {/* Add other fields as needed */}
+                </tr>
+              </thead>
+              <tbody>
+                {competitiveData.map((intel, index) => (
+                  <tr key={index + 1}>
+                    <td>{(currentPage - 1) * pageSize + index + 1}</td>
+                    <td>{intel.domain}</td>
+                    <td>{intel.competitorInfo}</td>
+                    {/* Add other fields as needed */}
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+          <div className="pagination-buttons">
+            {!isFirstPage && (
+              <button className="prev-button" onClick={handlePrevPage}>
+                &laquo; Prev
+              </button>
+            )}
+            {!isLastPage && (
+              <button className="next-button" onClick={handleNextPage}>
+                Next &raquo;
+              </button>
+            )}
+          </div>
+        </div>
       </div>
+      <Footer />
     </div>
-    <Footer />
-    </>
   );
 };
 
-export default AdminViewCI;
+export default CompetitiveIntelligencePortal;
