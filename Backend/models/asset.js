a06634800a155574e3592c0e1b4dccbf6e5e7bcf
Shashi-GTM <shashi@gtm4health.com>
2025-02-15 12:01:50 +0530

    Update asset.js
    Created Asset Model with fields

--- Backend/models/asset.js
+++ Backend/models/asset.js
@@ -1,2 +1,27 @@
 // A Model for Asset Tracking Module
-// Testing by Shashi
+// Testing by Shashi. Camel case being used.
+
+const mongoose = require('mongoose');
+
+const assetSchema = new mongoose.Schema({
+  productName:{
+    type:String
+  },
+  assetNumber: {
+    type: String,
+  },
+  emailID: {
+    //taken from user collection
+    type: String,
+  },
+observations: {
+  //to be added/updated by user.
+    type: String,
+  },
+  timestamp: {
+    type: Date,
+    default: Date.now, // Auto-generate timestamp if not provided
+  },
+});
+
+module.exports = mongoose.model('Asset', assetSchema);
