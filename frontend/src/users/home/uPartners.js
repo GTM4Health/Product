f6b87b9c7880290db67b2e3c80ce9e94379cd851
Sree Harsha GTM <sreeharsha@gtm4health.com>
2025-04-19 01:39:35 +0530

    fix: Adjust markup for "Products Managed" section in UPartners component

--- frontend/src/users/home/uPartners.js
+++ frontend/src/users/home/uPartners.js
@@ -72,7 +72,7 @@ const UPartners = () => {
                 {/* <li className="centre-insights-item"><h3 className="normal"><strong>Address:</strong> {partner.address}</h3></li> */}
                 <li className="centre-info-item"><h3 className="normal"><strong>Location:</strong> {partner.city}, {partner.state}</h3></li>
                 <li className="centre-info-item"><h3 className="normal"><strong>Role:</strong> {partner.role}</h3></li>
-                <li className="centre-insights-item"><h3 className="normal"><strong>Products Managed:</strong> {partner.products}</h3></li>
+                <li className="centre-insights-item"><h3 className="normal"><strong>Products Managed:</strong>  <br/> {partner.products}</h3></li>
                 <li className="centre-info-item"><h3 className="normal"><strong>Email:</strong> {partner.mail}</h3></li>
                 <li className="centre-info-item"><h3 className="normal"><strong>Phone:</strong> {partner.phone}</h3></li>
               </ul>
