5dc2b2c648211724b91a34962aad3affe231c4cf
Sree Harsha GTM <sreeharsha@gtm4health.com>
2025-06-21 18:10:49 +0530

    fix: Replace existing banners

--- frontend/src/users/home/LandingPage6.js
+++ frontend/src/users/home/LandingPage6.js
@@ -1,12 +1,14 @@
 import React, { useState, useEffect } from "react";
 import Footer from "../../layout/pages/Footer";
 import Header from "../../layout/users/Header";
-import Creative from "../../images/Creative.png";
-import Banner from "../../images/Banner.png";
+// import Creative from "../../images/Creative.png";
+// import Banner from "../../images/Banner.png";
 import Cases from "../../images/Cases.png";
+import Creative from "../../images/2025_Jun21_Banner1.png";
+import Banner from "../../images/2025_Jun21_Banner2.png";
 import "./Carousel.css"; // CSS code will be provided below
 
-const images = [Creative, Banner, Cases];
+const images = [Creative, Banner ];
 
 const LandingPage6 = () => {
   const [currentIndex, setCurrentIndex] = useState(0);
@@ -14,7 +16,7 @@ const LandingPage6 = () => {
   useEffect(() => {
     const interval = setInterval(() => {
       setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length);
-    }, 3000); // Auto-slide every 3 seconds
+    }, 6000); // Auto-slide every 3 seconds
 
     return () => clearInterval(interval);
   }, []);
